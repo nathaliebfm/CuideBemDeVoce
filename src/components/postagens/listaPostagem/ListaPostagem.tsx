1d774bb01312a7b959a88d4e4ce50c93fe664d32
nathaliebfm <natibfm@hotmail.com>
2022-11-18 16:28:56 -0300

    Algumas alterações nas páginas de lista

--- src/components/postagens/listaPostagem/ListaPostagem.tsx
+++ src/components/postagens/listaPostagem/ListaPostagem.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, ChangeEvent } from 'react'
 import { Link } from 'react-router-dom'
 import Postagem from '../../../model/Postagem'
 import { busca } from '../../../service/Service'
@@ -37,12 +37,28 @@ function ListaPostagem() {
     }
   }, [token])
 
+  const [postBuscado, setPostBuscado] = useState("")
+
+  function updateBusca(event: ChangeEvent<HTMLInputElement>) {
+    setPostBuscado(
+      event.target.value
+    )
+  }
+
   async function getPost() {
-    await busca("/postagens", setPosts, {
-      headers: {
-        'Authorization': token
-      }
-    })
+    if (postBuscado !== "") {
+      await busca(`/postagens/titulo/${postBuscado}`, setPosts, {
+        headers: {
+          Authorization: token
+        }
+      })
+    } else {
+      await busca("/postagens", setPosts, {
+        headers: {
+          'Authorization': token
+        }
+      })
+    }
   }
 
   useEffect(() => {
@@ -53,62 +69,67 @@ function ListaPostagem() {
 
   return (
     <>
+      <div className="group">
+        <input placeholder="Search" type="search" className="input" name='busca' onChange={(event: ChangeEvent<HTMLInputElement>) => updateBusca(event)} />
+        <svg className="icon" onClick={getPost} aria-hidden="true" viewBox="0 0 24 24"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
+      </div>
+      
+      <Box className="containerLista">
+
       {posts.length === 0 && <div className="preloader-inner">
-        <span className="dot"></span>
-        <div className="dots">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>}
+          <span className="dot"></span>
+          <div className="dots">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>}
+        
 
-{
-  posts.map(post => (
-    <Box m={2} >
-      <Card variant="outlined">
-        <CardContent>
-          <Typography color="textSecondary" gutterBottom>
-            Postagens
-          </Typography>
-          <Typography variant="h5" component="h2">
-            {post.titulo}
-          </Typography>
-          <Typography variant="body2" component="p">
-            {post.texto}
-          </Typography>
-          <Typography variant="body2" component="p">
-            Postagem feita em: {new Date(Date.parse(post.data)).toLocaleString()}
-          </Typography>
-          <Typography variant="body2" component="p">
-            {post.tema?.descricao}
-          </Typography>
-          <Typography>
-            Postado por: {post.usuario?.nome}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Box display="flex" justifyContent="center" mb={1.5}>
+        {posts.map(post => (
+            <Box alignSelf="flex-start" >
+              <Card variant="outlined" className="postagens">
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                    {post.titulo}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    {post.texto}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    Postagem feita em: {new Date(Date.parse(post.data)).toLocaleString()}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    Tema: {post.tema?.descricao}
+                  </Typography>
+                  <Typography>
+                    Postado por: {post.usuario?.nome}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Box display="flex" justifyContent="center" mb={1.5}>
 
-            <Link to={`/formularioPost/${post.id}`} className="text-decorator-none" >
-              <Box mx={1}>
-                <Button variant="contained" size='small' className="btnAtualizar" >
-                  atualizar
-                </Button>
-              </Box>
-            </Link>
-            <Link to={`/deletarPost/${post.id}`} className="text-decorator-none">
-              <Box mx={1}>
-                <Button variant="contained" size='small' color="secondary" className="btnDeletar">
-                  deletar
-                </Button>
-              </Box>
-            </Link>
-          </Box>
-        </CardActions>
-      </Card>
-    </Box>
-  ))
-}
+                    <Link to={`/formularioPost/${post.id}`} className="text-decorator-none" >
+                      <Box mx={1}>
+                        <Button variant="contained" size='small' className="btnAtualizar" >
+                          atualizar
+                        </Button>
+                      </Box>
+                    </Link>
+                    <Link to={`/deletarPost/${post.id}`} className="text-decorator-none">
+                      <Box mx={1}>
+                        <Button variant="contained" size='small' color="secondary" className="btnDeletar">
+                          deletar
+                        </Button>
+                      </Box>
+                    </Link>
+                  </Box>
+                </CardActions>
+              </Card>
+            </Box>
+          ))
+        }
+      </Box>
     </>
   )
 }
--- src/components/temas/listaTema/ListaTema.tsx
+++ src/components/temas/listaTema/ListaTema.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, ChangeEvent } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
 import './ListaTema.css';
@@ -11,7 +11,7 @@ import { toast } from 'react-toastify';
 
 function ListaTema() {
   const [temas, setTemas] = useState<Tema[]>([])
-  const token = useSelector<TokenState,TokenState["tokens"]>(
+  const token = useSelector<TokenState, TokenState["tokens"]>(
     (state) => state.tokens
   )
   let navigate = useNavigate()
@@ -34,10 +34,29 @@ function ListaTema() {
     }
   }, [token])
 
+
+  const [temaBuscado, setTemaBuscado] = useState("")
+
+  function updateBusca(event: ChangeEvent<HTMLInputElement>) {
+    setTemaBuscado(
+      event.target.value
+    )
+  }
+
   async function getTema() {
-    await busca("/temas", setTemas, {
-      headers: { "Authorization": token }
-    })
+    if(temaBuscado !== ''){
+      await busca(`/temas/descricao/${temaBuscado}`, setTemas, {
+        headers: {
+          Authorization: token
+        }
+      })
+    } else {
+      await busca('/temas', setTemas, {
+        headers: {
+          Authorization: token
+        }
+      })
+    }
   }
 
   useEffect(() => {
@@ -47,39 +66,44 @@ function ListaTema() {
 
   return (
     <>
+      <div className="group">
+      <input placeholder="Search" type="search" className="input" name='busca' onChange={(event: ChangeEvent<HTMLInputElement>) => updateBusca(event)} />
+      <svg className="icon" onClick={getTema} aria-hidden="true" viewBox="0 0 24 24"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
+    </div>
+
       {
         temas.map(tema => (
-        <Box m={2} >
-          <Card variant="outlined">
-            <CardContent>
-              <Typography color="textSecondary" gutterBottom>
-                Tema
-              </Typography>
-              <Typography variant="h5" component="h2">
-                {tema.descricao}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Box display="flex" justifyContent="center" mb={1.5} >
+          <Box m={2} >
+            <Card variant="outlined">
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>
+                  Tema
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {tema.descricao}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Box display="flex" justifyContent="center" mb={1.5} >
 
-                <Link to={`/formularioTema/${tema.id}`} className="text-decorator-none">
-                  <Box mx={1}>
-                    <Button variant="contained" size='small' className="btnAtualizar">
-                      atualizar
-                    </Button>
-                  </Box>
-                </Link>
-                <Link to={`/deletarTema/${tema.id}`} className="text-decorator-none">
-                  <Box mx={1}>
-                    <Button variant="contained" size='small' style={{ backgroundColor: "#F18F01", color: "white" }} className="btnDeletar">
-                      deletar
-                    </Button>
-                  </Box>
-                </Link>
-              </Box>
-            </CardActions>
-          </Card>
-        </Box>
+                  <Link to={`/formularioTema/${tema.id}`} className="text-decorator-none">
+                    <Box mx={1}>
+                      <Button variant="contained" size='small' className="btnAtualizar">
+                        atualizar
+                      </Button>
+                    </Box>
+                  </Link>
+                  <Link to={`/deletarTema/${tema.id}`} className="text-decorator-none">
+                    <Box mx={1}>
+                      <Button variant="contained" size='small' style={{ backgroundColor: "#F18F01", color: "white" }} className="btnDeletar">
+                        deletar
+                      </Button>
+                    </Box>
+                  </Link>
+                </Box>
+              </CardActions>
+            </Card>
+          </Box>
         ))
       }
     </>
